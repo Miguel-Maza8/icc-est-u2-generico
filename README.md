b39b007dedb0424d9f9648ab0d43ba657dbd6200
Miguel Maza <maza68206@gmail.com>
2026-06-03 09:05:05 -0500

    Commit P

--- README.md
+++ README.md
@@ -1,18 +1,26 @@
-## Getting Started
+# Práctica: Clases Genéricas en Java
 
-Welcome to the VS Code Java world. Here is a guideline to help you get started to write Java code in Visual Studio Code.
+## Datos del Estudiante
+- **Nombre:** Miguel Mza
+- **Curso:** Computacion grupo 1
+- **Fecha:** 3-06-2026
 
-## Folder Structure
+---
+## 1. Implementación de Caja<T> y Par<K, V>
 
-The workspace contains two folders by default, where:
 
-- `src`: the folder to maintain sources
-- `lib`: the folder to maintain dependencies
+**Fecha:** 3/6/2026
 
-Meanwhile, the compiled output files will be generated in the `bin` folder by default.
+**Descripción:** En esta práctica se implementaron las clases genéricas Caja<T> y Par<K, V> dentro del paquete models. La clase Caja<T> permite almacenar y obtener un dato de cualquier tipo, mientras que la clase Par<K, V> permite representar una relación entre una clave y un valor. En la captura se muestra la ejecución del programa en consola con diferentes tipos de datos.
 
-> If you want to customize the folder structure, open `.vscode/settings.json` and update the related settings there.
+Imagen de impresion en consola
 
-## Dependency Management
+![alt text](image.png)
 
-The `JAVA PROJECTS` view allows you to manage your dependencies. More details can be found [here](https://github.com/microsoft/vscode-java-dependency#manage-dependencies).
+Imagen del codigo 
+
+![alt text](image-1.png)
+![alt text](image-2.png)
+
+**Descipcion: **  En la practica se implemento las clases genericas
+
--- src/App.java
+++ src/App.java
@@ -1,5 +1,49 @@
-public class App {
+
+
+import javax.crypto.spec.PBEKeySpec;
+
+import models.Caja;
+import models.Par;
+
+public class App { 
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       Caja<String> stringCaja = new Caja<>();
+       Caja<Integer> integerCaja = new Caja<>();
+       Caja<Double> doubleCaja = new Caja<>();
+       Caja<Boolean> booleanCaja = new Caja<>();
+
+       stringCaja.setT("Hola mundo");
+       integerCaja.setT(80);
+       doubleCaja.setT(10.5);
+       booleanCaja.setT(false);
+
+       Par<Integer,String> datosPar = new Par<>();
+       Par<String,Integer> datosPar2 = new Par<>();
+       Par<String,Double> datosPar3 = new Par<>();
+
+       datosPar.setK(1);
+       datosPar.setV("Evelyn Perez");
+       datosPar2.setK("Edad");
+       datosPar2.setV(21);
+       datosPar3.setK("Promedio");
+       datosPar3.setV(9.4);
+
+
+
+
+       System.out.println("-----Uso de Caja<T>-----");
+       System.out.println("Caja de texto : " + stringCaja.getT());
+       System.out.println("Caja de entero : " + integerCaja.getT());
+       System.out.println("Caja de decimal : " + doubleCaja.getT());
+       System.out.println("¿ La caja de texto esta vacia: ? " + booleanCaja.getT());
+       System.out.println();
+
+       System.out.println("-----Uso de Par<K, V>-----");
+       System.out.println("Clave : " + datosPar.getK()+ " | " + "Valor: " + datosPar.getV());
+       System.out.println("Clave : " + datosPar2.getK()+ " | " + "Valor: " + datosPar2.getV());
+       System.out.println("Clave : " + datosPar3.getK()+ " | " + "Valor: " + datosPar3.getV());
+
+
+
     }
 }
